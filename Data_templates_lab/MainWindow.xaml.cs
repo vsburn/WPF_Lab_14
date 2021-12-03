f92ddb72a1a14a83f8f9c6a97fe84caa7fececda
Fcss Void <fcss.void@gmail.com>
2021-12-03 22:18:00 +0300

    Добавьте файлы проекта.

---
+++ Data_templates_lab/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace Data_templates_lab
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                Name = "Холодильник",
+                Price = 990,
+                PathImg = "img/refrigerator.png",
+                Category = Category.Appliances
+            });
+            products.Add(new Product()
+            {
+                Name = "Микроволновка",
+                Price = 300,
+                PathImg = "img/oven.png",
+                Category = Category.Appliances
+            });
+            products.Add(new Product()
+            {
+                Name = "Молоко",
+                Price = 2.90,
+                PathImg = "img/milk.png",
+                Category = Category.Food
+            });
+            products.Add(new Product()
+            {
+                Name = "Сыр",
+                Price = 15.99,
+                PathImg = "img/cheese.png",
+                Category = Category.Food
+            });
+            products.Add(new Product()
+            {
+                Name = "Чайник",
+                Price = 150,
+                PathImg = "img/kettle.png",
+                Category = Category.Appliances
+            });
+            listBox.ItemsSource = products;
+        }
+    }
+
+
+}
---
+++ Data_templates_lab/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_templates_lab
+{
+    /// <summary>
+    ///Определить класс Product для хранения информации о товаре.
+    /// Класс должен содержать характеристики:
+    /// Название товара (строка)
+    /// Цена (число)
+    /// Изображение (строка - путь к файлу с изображением)
+    /// Категория (перечисление, возможные значения – Еда, Бытовая техника)
+    /// Разработать шаблон данных для отображения списка товаров в компоненте ListBox
+    /// При помощи триггеров определите разный шаблон для товаров категории Еда и категории Бытовая техника.
+    /// </summary>
+    
+    public enum Category
+    {
+        Food,
+        Appliances
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string PathImg { get; set; }
+        public Category Category { get; set; }
+    }
+}
